90363d56eecf1e7932240d3d8e5bccffd59d7ec8
Terry Mace <terry.mace@payroc.com>
2022-01-13 22:49:04 +0000

    Replicated example given in Binance readme

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Binance.Spot;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var websocket = new MarketDataWebSocket("btcusdt@aggTrade");
+
+        websocket.OnMessageReceived(
+            (data) =>
+            {
+                Console.WriteLine(data);
+
+                return Task.CompletedTask;
+            }, CancellationToken.None);
+
+        await websocket.ConnectAsync(CancellationToken.None);
+    }
+}
